513a6229d4fe15ffb86cdda7bfcb0b00df4e3001
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-18 20:19:53 +0700

    Create inputNumber.py

---
+++ inputNumber.py
@@ -0,0 +1,8 @@
+#input nhan gia tri string nen phai convert to integer de compare with 0
+a = int(input("Ur number: "))
+if a > 0:
+    print("number is positive")
+elif a<0:
+    print("number is negative")
+else:
+    print("number is zero")
