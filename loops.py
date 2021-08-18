e2e94739e031074ffb977b663753324da1577167
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-18 20:19:55 +0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,14 @@
+# tao vong lap chay tu 0-5 theo tung dong
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i) 
+# tao vong lap theo range chay 6 ky tu tu 0-5
+for i in range(6):
+    print(i)
+names = ["Nguyen", "Nguyen2", "Nguyen1"]
+# tao vong lap loop list names
+for i in names:
+    print(i)
+name = "Nguyen"
+# lap lai tat ca cac chu trong string name
+for character in name:
+    print(character)
