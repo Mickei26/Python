d051658856c4a201677bea0ffd9a5166732053e7
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-20 18:42:15 +0700

    Create mainSquare.py

---
+++ mainSquare.py
@@ -0,0 +1,10 @@
+from func import square
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+    
+# cach 2 de import function
+import func
+
+for i in range(10):
+    print(f"The square of {i} is {func.square(i)}") 
