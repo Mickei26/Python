d3c3c6495c1661e5771954342d8760591df5ba6c
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-22 10:37:41 +0700

    update

--- pyWebTest/hello/views.py
+++ pyWebTest/hello/views.py
@@ -1,7 +1,6 @@
-from pyWebTest import hello
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "hello/index.html")
+    return render(request, "clinet/index.html")
