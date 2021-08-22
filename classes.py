9fe858b524f2ff1b4a15312a6e9f212fe9d846ad
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-22 09:23:29 +0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -8,7 +8,27 @@ p = Point(2, 8)
 print(p.x)
 print(p.y)
 
+
 class Flight():
-    def __init__(self, ca):
-        self.ca
-        
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def addPassenger(self, name):
+        if not self.openSets():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def openSets(self):
+        return self.capacity - len(self.passengers)
+
+
+flight = Flight(3)
+people = ["Nguyen", "Nguyen1", "Nguyen2", "Nguyen3"]
+for person in people:
+    success = flight.addPassenger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seat for {person}.")
