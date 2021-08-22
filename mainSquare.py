39a28b256a61c17b0c10a42a1c55966505c3dec0
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-22 09:23:53 +0700

    Update mainSquare.py

--- mainSquare.py
+++ mainSquare.py
@@ -1,10 +1,10 @@
+import func
 from func import square
 
 for i in range(10):
     print(f"The square of {i} is {square(i)}")
-    
+
 # cach 2 de import function
-import func
 
 for i in range(10):
-    print(f"The square of {i} is {func.square(i)}") 
+    print(f"The square of {i} is {func.square(i)}")
